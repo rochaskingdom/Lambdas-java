00abb31d56eafba27ec9ac18d40b7dbca098720f
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-09-13 12:46:24 -0300

    Function

---
+++ src/Funcao.java
@@ -0,0 +1,25 @@
+import java.util.function.Function;
+
+public class Funcao {
+
+    public static void main(String[] args) {
+
+        Function<Integer, String> parOuImpar = numero -> numero % 2 == 0 ? "Par" : "Ímpar";
+
+        Function<String, String> oResultadoE = valor -> "O resultado é: " + valor;
+
+        Function<String, String> empolgado = valor -> valor + "!!!";
+
+        Function<String, String> duvida = valor -> valor + "???";
+
+        String resultadoFinal1 = parOuImpar.andThen(oResultadoE).andThen(empolgado).apply(30);
+
+        System.out.println(resultadoFinal1);
+
+        String resultadoFinal2 = parOuImpar.andThen(oResultadoE).andThen(duvida).apply(31);
+
+        System.out.println(resultadoFinal2);
+
+        System.out.println(parOuImpar.apply(30));
+    }
+}
