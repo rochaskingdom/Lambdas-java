e48188fc4f52f4e4c63c73291d1a28280f55ad7a
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-09-13 03:30:25 -0300

    Interfaces Funcionais #02

--- src/Calculo.java
+++ src/Calculo.java
@@ -1,4 +1,12 @@
 @FunctionalInterface
 public interface Calculo {
     double executar(double a, double b);
+
+    default String legal() {
+        return "legal";
+    }
+
+    static String muitoLegal() {
+        return "muito legal";
+    }
 }
